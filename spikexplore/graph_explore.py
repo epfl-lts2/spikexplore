c85043f64271025f5e4b787bfc44d5d1c2f3d49d
Nicolas Aspert <nicolas.aspert@epfl.ch>
2021-04-01 15:21:04 +0200

    refactor configuration into object

--- spikexplore/graph_explore.py
+++ spikexplore/graph_explore.py
@@ -3,10 +3,10 @@ from spikexplore.collect_edges import spiky_ball
 
 
 def create_graph(backend, nodes_df, edges_df, nodes_info, config):
-    min_weight = config.get('min_weight', 1)
+    min_weight = config.min_weight
     g = graph_from_edgeslist(edges_df, min_weight=min_weight)
     g = backend.add_graph_attributes(g, nodes_df, edges_df, nodes_info)
-    g = reduce_graph(g, config['min_degree'])
+    g = reduce_graph(g, config.min_degree)
     g = handle_spikyball_neighbors(g, backend)
     return g
 
@@ -16,9 +16,9 @@ def explore(backend, initial_nodes, config):
         raise ValueError('Cannot start without initial nodes.')
     nodes_list, nodes_df, edges_df, nodes_info = spiky_ball(initial_nodes,
                                                             backend,
-                                                            config['collection_settings'],
+                                                            config.data_collection,
                                                             node_acc=backend.get_node_info()
                                                             )
     # create graph from edge list
-    g = create_graph(backend, nodes_df, edges_df, nodes_info, config['graph'])
+    g = create_graph(backend, nodes_df, edges_df, nodes_info, config.graph)
     return g
